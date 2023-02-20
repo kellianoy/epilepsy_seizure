7efe7fa4a1d54e450311d1c158ebacc7c3c4b34e
Mathys Noir <mathys.noir@edu.ece.fr>
2023-02-20 17:27:28 +0100

    feat: start workflow and end of dummy

--- problem.py
+++ problem.py
@@ -1,23 +1,50 @@
 import rampwf as rf
 import numpy as np
+import pandas as pd
+from rampwf.utils.importing import import_module_from_source
+from sklearn.model_selection import ShuffleSplit
 import os
 
 problem_title = 'Predection of seizure'
 
-_prediction_label_name = [1, 2, 3, 4]  # TODO
+_prediction_label_name = [1, 2] # TODO
 
-Prediction = rf.prediction_types.make_multiclass(
-    label_names=_prediction_label_name)
+Prediction = rf.prediction_types.make_multiclass(label_names=_prediction_label_name*1)
 
-workflow = rf.workflows.Estimator()  # TODO
+class SeizureClassifierWorkflow:
+    def __init__(self, workflow_element_names=['seizure_classifier.py']):
+        self.workflow_element_names = workflow_element_names
+        self.estimator = None
 
-score_types = []  # TODO
+    def train_submission(self, module_path, X_train, y_train):
+        estimator_module = import_module_from_source(
+            os.path.join(module_path, self.workflow_element_names[0]),
+            self.workflow_element_names[0],
+            sanitize=True)
 
+        model = estimator_module.SeizureClassifier()
+        model.fit(X_train, y_train)
+        
+        return model
+    
+    def test_submission(self, module_path, model, X_test):
+        y_pred = model.predict(X_test)
+        return y_pred
+
+workflow = SeizureClassifierWorkflow()
+
+score_types = [
+    rf.score_types.ROCAUC(name='auc'),
+    rf.score_types.Accuracy(name='acc'),
+] # TODO
 
 def get_cv(X, y):
-    # TODO
-    pass
+    cv = ShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+    return cv.split(X, y)
 
+def _read_data(path, f_name):
+    fname = f"data_{f_name}"
+    data = pd.read_csv(os.path.join(path, 'data', fname), index_col=0)
 
 def _read_data(path, dataset):
     """Read data from the numpy arrays
