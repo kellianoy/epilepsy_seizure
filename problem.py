645f6cb85b3a55b055e5d0a423558bd740957ddc
Mathys Noir <mathys.noir@edu.ece.fr>
2023-02-20 17:27:28 +0100

    feat: start workflow and end of dummy

--- problem.py
+++ problem.py
@@ -1,16 +1,36 @@
 import rampwf as rf
 import pandas as pd
+from rampwf.utils.importing import import_module_from_source
 import os
 
 problem_title = 'Predection of seizure'
 
-_prediction_label_name = [1, 2, 3, 4] # TODO
+_prediction_label_name = [1, 2] # TODO
 
-Prediction = rf.prediction_types.make_multiclass(label_names=_prediction_label_name)
+Prediction = rf.prediction_types.make_multiclass(label_names=_prediction_label_name*[1])
+
+class SeizureClassifierWorkflow:
+    def __init__(self, workflow_element_names=['seizure_classifier.py']):
+        self.workflow_element_names = workflow_element_names
+        self.estimator = None
+
+    def train_submission(self, module_path, X_train, y_train):
+        estimator_module = import_module_from_source(
+            os.path.join(module_path, self.workflow_element_names[0]),
+            self.workflow_element_names[0],
+            sanitize=True)
+
+
+        # TODO
+        pass
+
 
 workflow = rf.workflows.Estimator() # TODO
 
-score_types = [] # TODO
+score_types = [
+    rf.score_types.roc_auc(name='auc'),
+    rf.score_types.Accuracy(name='acc'),
+] # TODO
 
 def get_cv(X, y):
     # TODO
