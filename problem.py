3e4acda254317ccdcbbe956f1318a8f763ec4a97
Kellian <kellian.cottart@edu.ece.fr>
2023-02-21 18:38:07 +0100

    feat: only one dataset

--- problem.py
+++ problem.py
@@ -1,38 +1,61 @@
 import rampwf as rf
-import pandas as pd
+import numpy as np
 import os
 
 problem_title = 'Predection of seizure'
 
-_prediction_label_name = [1, 2, 3, 4] # TODO
+_prediction_label_name = [1, 2, 3, 4]  # TODO
 
-Prediction = rf.prediction_types.make_multiclass(label_names=_prediction_label_name)
+Prediction = rf.prediction_types.make_multiclass(
+    label_names=_prediction_label_name)
 
-workflow = rf.workflows.Estimator() # TODO
+workflow = rf.workflows.Estimator()  # TODO
 
-score_types = [] # TODO
+score_types = []  # TODO
+
 
 def get_cv(X, y):
     # TODO
     pass
 
-def _read_data(path, f_name):
-    fname = f"data_{f_name}"
-    data = pd.read_csv(os.path.join(path, 'data', fname), index_col=0)
 
-    fname = f"labels_{f_name}"
-    labels = pd.read_csv(os.path.join(path, 'data', fname), index_col=0)
+def _read_data(path, dataset):
+    """Read data from the numpy arrays
+    Parameters
+    ----------
+    path: str
+        The path to the data folder
+    dataset: str
+        The name of the dataset to load - either 'train' or 'test'
+    Returns
+    ----------
+    X: np.ndarray
+        Data of the specified patients
+    y: np.ndarray
+        Labels of the specified patients
+    """
+    # Check that the path exists
+    if not os.path.exists(path):
+        raise ValueError('The path {} does not exist'.format(path))
+    if not len(patients):
+        raise ValueError('No patient specified')
+    if dataset not in ['train', 'test']:
+        raise ValueError('Unknown dataset: {}'.format(dataset))
+    X_filename, y_filename = f"X_{dataset}.npy", f"y_{dataset}.npy"
+    X_path, y_path = os.path.join(
+        path, X_filename), os.path.join(path, y_filename)
+    # Check that the files exist
+    if not os.path.exists(X_path):
+        raise ValueError('The path {} does not exist'.format(X_path))
+    if not os.path.exists(y_path):
+        raise ValueError('The path {} does not exist'.format(y_path))
+    # Load the data
+    return np.load(X_path), np.load(y_path)
 
-    test = os.getenv('RAMP_TEST_MODE', 0)
-    if test:
-        N_SMALL = 100 # TODO change
-        data = data.iloc[:N_SMALL]
-        labels = labels.iloc[:N_SMALL]
-    
-    return data, labels
 
-def get_train_data(path='.'):
-    return _read_data(path, 'train.csv')
+def get_train_data(path='./dataset'):
+    return _read_data(path, 'train')
 
-def get_test_data(path='.'):
-    return _read_data(path, 'test.csv')
+
+def get_test_data(path='./dataset'):
+    return _read_data(path, 'test')
