b3fc5e5befdf0c6849e2771eb53c6820cfa0f022
Mathys NOIR <mathys.noir@edu.ece.fr>
2023-02-25 01:21:54 +0100

    feat: rocauc

--- submissions/starting_kit/eegclassifier.py
+++ submissions/starting_kit/eegclassifier.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-class SeizureClassifier(object):
+class EegClassifier(object):
     def __init__(self):
         pass
 
-    def fit(self, data: list, y):
+    def fit(self, data, y):
         classes = [1,2]
         self.n_classes = len(classes)
         pass
 
-    def predict(self, data: list):
+    def predict(self, data):
         proba = np.random.rand(len(data), self.n_classes)
         proba /= proba.sum(axis=1)[:, np.newaxis]
         return proba
